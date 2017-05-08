fbe0fd8d55c470d23a32da319c1862253f84ad00
CLoganSamuels <christopherlogansamuels@yahoo.com>
2017-05-08 15:38:13 -0500

    Update urls.py

--- codeSample/urls.py
+++ codeSample/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('chem.urls')),
-]
+] + static(settings.STATIC_URL,
+document_root=settings.STATIC_ROOT)
